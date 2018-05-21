c53af1808dd715f5bb1e6ffed1431cd22e8412bd
Korey Earl <korey.earl@gmail.com>
2018-05-21 07:46:16 -0500

    Adding in some JSX for

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+  firstName: 'Korey',
+  lastName: 'Earl'
+}
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>
+  } 
+  return <h1>Hello, Stranger!</h1>
+}
+
+const element = getGreeting(user);
+
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  element,
   document.getElementById('root')
 );
